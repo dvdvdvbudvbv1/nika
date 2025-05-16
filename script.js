a6eab62f401e48265f5d7b7923a9b3065f83a998
dvdvdvbudvbv1 <dvdvdvbudvbv@gmail.com>
2025-05-16 08:31:46 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const button = document.getElementById("openBtn");
 const message = document.querySelector(".message");
 
@@ -5,12 +6,10 @@ button.addEventListener("click", () => {
   message.classList.remove("hidden");
   message.classList.add("show");
 
-  // Конфетти
   for (let i = 0; i < 100; i++) {
     createConfetti();
   }
 
-  // Вибрация
   if (navigator.vibrate) {
     navigator.vibrate(200);
   }
@@ -22,11 +21,9 @@ function createConfetti() {
   confetti.style.left = Math.random() * 100 + "vw";
   confetti.style.animationDuration = 2 + Math.random() * 3 + "s";
   document.body.appendChild(confetti);
-
   setTimeout(() => confetti.remove(), 5000);
 }
 
-// CSS конфетти
 const style = document.createElement("style");
 style.textContent = `
   .confetti {
